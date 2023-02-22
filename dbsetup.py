13c54e772d15a18cc14507671e30ea137e3d9208
Martin Laubscher <martin.laubscher@me.com>
2023-02-22 19:57:46 +0000

    updated scraper and dbsetup - scraper: writes to database now - dbsetup: column 'last update' in table 'availability' is now type DATETIME - improved comments

--- dbsetup.py
+++ dbsetup.py
@@ -1,28 +1,23 @@
-import sqlalchemy as sqla
 from sqlalchemy import create_engine
 from sqlalchemy import text
-import traceback
-import glob
-import os
-from pprint import pprint
-import simplejson as json
-import requests
-import time
-from IPython.display import display
 import dblogin
 
+# creating the engine with connection parameters stored in separate dblogin file
 engine = create_engine(
     "mysql+mysqldb://{}:{}@{}:{}".format(dblogin.USER, dblogin.PASSWORD, dblogin.URI, dblogin.PORT),
     echo=True)
 
+# preparing sql statement to create db
 sql = """
 CREATE DATABASE IF NOT EXISTS dbikes;
 """
 
+# creating the db and "activating it"
 with engine.begin() as connection:
     connection.execute(text(sql))
     connection.execute(text('USE dbikes'))
 
+# preparing sql statement to create station table
 sql = """
 CREATE TABLE IF NOT EXISTS station (
 address VARCHAR(256),
@@ -37,6 +32,8 @@ position_lng REAL,
 status VARCHAR(256)
 );
 """
+
+# create station table
 try:
     with engine.begin() as connection:
         connection.execute(text("DROP TABLE IF EXISTS station;"))
@@ -45,18 +42,20 @@ try:
 except Exception as e:
     print(e)
 
-
+# preparing sql statement to create availability table
 sql = """
 CREATE TABLE IF NOT EXISTS availability(
 number INTEGER,
 available_bikes INTEGER,
 available_bike_stands INTEGER,
-last_update INTEGER
+last_update DATETIME
 );
 """
+
+# creating availability table
 try:
     with engine.begin() as connection:
         res = connection.execute(text(sql))
         print(res.fetchall())
 except Exception as e:
-    print(e) #traceback.format_exc())
+    print(e)  # traceback.format_exc())
--- scraper.py
+++ scraper.py
@@ -1,13 +1,10 @@
 import json
 import requests
 import traceback
-import datetime
-import time
 import apilogin
 import sqlalchemy as sqla
 from sqlalchemy import create_engine
 from sqlalchemy import insert
-from sqlalchemy import text
 import datetime
 import dblogin
 
@@ -16,40 +13,45 @@ engine = create_engine(
     "mysql+mysqldb://{}:{}@{}:{}/{}".format(dblogin.USER, dblogin.PASSWORD, dblogin.URI, dblogin.PORT, dblogin.DB),
     echo=True)
 
+# creating metadata objects for each of the tables
 metadataS = sqla.MetaData()
 metadataA = sqla.MetaData()
 
+# creating table object for station table
 station = sqla.Table("station", metadataS,
                      autoload_with=engine,
                      schema='dbikes'
                      )
 
+# creating table object for availability table
 availability = sqla.Table("availability", metadataA,
                           autoload_with=engine,
                           schema='dbikes'
                           )
 
+# API parameters (apilogin is a separate py file)
 APIKEY = apilogin.APIKEY
 NAME = 'Dublin'
 STATIONS = 'https://api.jcdecaux.com/vls/v1/stations'
 
 
-def write_to_file(text, now):
-    with open('data/bikes_{}'.format(now).replace(' ', '_'), 'w') as f:
-        f.write(text)
-
-
 def write_to_db(text):
+    # loading the data
     data = json.loads(text)
+    # starting transaction
     with engine.begin() as connection:
+        # preparing statement for insertion
         stmt = tuple(map(fix_data, data))
+        # executing insert operations for each of the tables (station & availability)
         connection.execute(insert(station), stmt)
         connection.execute(insert(availability), stmt)
 
 
+# function to "fix" data so it can be processed in the db without issues
 def fix_data(data):
     data['position_lat'] = data['position']['lat']
     data['position_lng'] = data['position']['lng']
+    # converting epoch time in milliseconds to date-time format
     data['last_update'] = datetime.datetime.fromtimestamp(data['last_update'] / 1000)
     return data
 
@@ -57,9 +59,9 @@ def fix_data(data):
 def main():
     try:
         now = datetime.datetime.now()
+        # getting the data from the api
         r = requests.get(STATIONS, params={'apiKey': APIKEY, 'contract': NAME})
-        print(r, now)
-        write_to_file(r.text, now)
+        #calling function to write data to api
         write_to_db(r.text)
     except:
         print(traceback.format_exc())
