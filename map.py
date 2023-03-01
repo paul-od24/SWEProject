b691d4a601cefc0d42302a85dc52ea3b34b8aaf6
Martin Laubscher <martin.laubscher@me.com>
2023-03-01 05:42:19 +0000

    map showing station pins

---
+++ map.py
@@ -0,0 +1,44 @@
+import json
+
+from flask import Flask, render_template, jsonify
+import sqlalchemy as sqla
+from sqlalchemy import create_engine, select
+import pandas as pd
+import dblogin
+import apilogin
+
+# creating the engine
+engine = create_engine(
+    "mysql+mysqldb://{}:{}@{}:{}/{}".format(dblogin.USER, dblogin.PASSWORD, dblogin.URI, dblogin.PORT, dblogin.DB),
+    echo=True)
+
+metadataS = sqla.MetaData()
+# creating table object for station table
+station = sqla.Table("station", metadataS,
+                     autoload_with=engine,
+                     schema='dbikes'
+                     )
+
+stmt = select(station.c.name, station.c.position_lat, station.c.position_lng)
+
+pinDic = {}
+
+with engine.begin() as connection:
+    for row in connection.execute(stmt):
+        pos = {"lat": float(row.position_lat), "lng": float(row.position_lng)}
+        pinDic[row.name] = pos
+
+print(str(pinDic))
+
+app = Flask(__name__, template_folder="./templates")
+
+
+@app.route("/")
+def mapview():
+    # pins= json.dumps()
+    # pins.status_code = 200
+    return render_template('index.html', dic=json.dumps(pinDic), mapkey=apilogin.MAPKEY)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
