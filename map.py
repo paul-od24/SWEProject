586609097fb531bf2d35ccddf6cba62a63317c8c
DanielO-Reilly <117649568+DanielO-Reilly@users.noreply.github.com>
2023-03-05 00:04:10 +0000

    Weather Info prototype created

--- map.py
+++ map.py
@@ -2,7 +2,7 @@ import json
 
 from flask import Flask, render_template, jsonify
 import sqlalchemy as sqla
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine, select, text
 import pandas as pd
 import dblogin
 import apilogin
@@ -30,6 +30,40 @@ with engine.begin() as connection:
 
 print(str(pinDic))
 
+# creating metadata objects for each of the tables
+metadataWH = sqla.MetaData()
+metadataWF = sqla.MetaData()
+
+# creating table object for weather_historical table
+weather_historical = sqla.Table("weather_historical", metadataWH,
+                     autoload_with=engine,
+                     schema='dbikes'
+                     )
+
+# creating table object for weather_forecast table
+weather_forecast = sqla.Table("weather_forecast", metadataWF,
+                          autoload_with=engine,
+                          schema='dbikes'
+                          )
+
+wCur = {}
+stmt = "SELECT * FROM weather_historical ORDER BY `time` DESC LIMIT 1"
+with engine.begin() as connection:
+        res = connection.execute(text(stmt))
+        res = res.mappings().all()
+        res = res[0]
+        data = {"symbol": res.symbol, "rain": res.rain}
+        wCur = data
+        
+stmt = select(weather_forecast.c.end, weather_forecast.c.symbol, weather_forecast.c.rain_hourly)
+
+wetDic = {}
+
+with engine.begin() as connection:
+    for row in connection.execute(stmt):
+        data = {"symbol": row.symbol, "rain": row.rain_hourly}
+        wetDic[str(row.end)] = data
+
 app = Flask(__name__, template_folder="./templates")
 
 
@@ -37,7 +71,7 @@ app = Flask(__name__, template_folder="./templates")
 def mapview():
     # pins= json.dumps()
     # pins.status_code = 200
-    return render_template('index.html', dic=json.dumps(pinDic), mapkey=apilogin.MAPKEY)
+    return render_template('index.html', dic=json.dumps(pinDic), mapkey=apilogin.MAPKEY, wCur=json.dumps(wCur), wetDic=json.dumps(wetDic))
 
 
 if __name__ == "__main__":
--- static/js/index.js
+++ static/js/index.js
@@ -27,4 +27,81 @@ function initMap() {
   }
 }
 
+function popWeatherCurrent(weatherDict) {
+  let weather = '<th colspan="';
+  weather += "2";
+  weather += '">Current Weather</th>';
+  weather += '<tr><td>';
+  weather += weatherDict["symbol"];
+  weather += '</td><td>';
+  weather += weatherDict["rain"];
+  weather += '</td></tr>';
+  document.getElementById("weather_cur").innerHTML = weather;
+  }
+
+function popWeather48(weatherDict) {
+  let weather = '<th colspan="';
+  weather += "3";
+  weather += '">Weather Next 48hrs</th>';
+  let rows = [];
+  for (let i = 0; i < 3; ++i) {
+    rows[i] = '<tr>';
+  }
+  let i = 0;
+  for (var key in weatherDict) {
+    splitKey = key.split(' ');
+    for (let j = 0; j < rows.length; ++j) {
+      rows[j] += '<td>';
+    }
+    rows[0] += splitKey[1];
+    rows[1] += weatherDict[key]['symbol'];
+    rows[2] += weatherDict[key]['rain'];
+    for (let j = 0; j < rows.length; ++j) {
+      rows[j] += '</td>';
+    }
+    if (i >= 48) {
+      break;
+    }
+    i++;
+  }
+  for (let j = 0; j < rows.length; ++j) {
+    rows[j] += '</tr>';
+  }
+  for (let j = 0; j < rows.length; ++j) {
+    weather += rows[j];
+  }
+  document.getElementById("weather_48").innerHTML = weather;
+}
+
+function popWeatherWeek(weatherDict) {
+  let weather = '<th colspan="';
+  weather += "4";
+  weather += '">Weather Next Week</th>';
+  let rows = [];
+  for (let i = 0; i < 3; ++i) {
+    rows[i] = '<tr>';
+  }
+  for (var key in weatherDict) {
+    splitKey = key.split(' ');
+    if (splitKey[1] == '12:00:00') {
+      for (let j = 0; j < rows.length; ++j) {
+        rows[j] += '<td>';
+      }
+      rows[0] += splitKey[0].split('-')[2];
+      rows[1] += weatherDict[key]['symbol'];
+      rows[2] += weatherDict[key]['rain'];
+      for (let j = 0; j < rows.length; ++j) {
+        rows[j] += '</td>';
+      }
+    }
+  }
+  for (let j = 0; j < rows.length; ++j) {
+    rows[j] += '</tr>';
+  }
+  for (let j = 0; j < rows.length; ++j) {
+    weather += rows[j];
+  }
+  document.getElementById("weather_week").innerHTML = weather;
+}
+
 window.initMap = initMap;
